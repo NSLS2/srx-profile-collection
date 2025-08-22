4086c9f208cdbc30cd8c5875659537adee8ce1cc
SRX Operator <xf05id1@xf05id2-ws7.nsls2.bnl.local>
2025-08-22 16:46:16 -0400

    Fixed md passing in modded functions and added modded grid_scan plan

--- startup/64-xrdscans.py
+++ startup/64-xrdscans.py
@@ -568,12 +568,12 @@ def static_xrd(num,
 
 # Run-agnostic standard scans
 
-def agnostic_run_decorator(run_agnostic):
+def agnostic_run_decorator(run_agnostic, md=None):
     def decorator(func):
         if run_agnostic:
             return func
         else:
-            return run_decorator(func)
+            return run_decorator(func, md=md)
     return decorator
 
 
@@ -619,8 +619,9 @@ def mod_count(
     """
     _check_detectors_type_input(detectors)
     _md = {}
-    if md is None:
+    if md is None and not run_agnostic:
         md = get_stock_md({})
+        get_det_md(md, [detectors])
     _md.update(md or {})
 
     # per_shot might define a different stream, so do not predeclare primary
@@ -628,7 +629,7 @@ def mod_count(
     msg_per_step: PerShot = per_shot if per_shot else bps.one_shot
 
     @stage_decorator(detectors)
-    @agnostic_run_decorator(run_agnostic)
+    @agnostic_run_decorator(run_agnostic, md=md)
     def inner_count() -> MsgGenerator[str]:
         if predeclare:
             yield from bps.declare_stream(*detectors, name="primary")
@@ -685,8 +686,9 @@ def mod_list_scan(
         raise ValueError("The list of arguments must contain a list of points for each defined motor")
 
     _md = {}
-    if md is None:
+    if md is None and not run_agnostic:
         md = get_stock_md({})
+        get_det_md(md, [detectors])
     _md.update(md or {})
 
     # set some variables and check that all lists are the same length
@@ -756,8 +758,9 @@ def mod_scan_nd(
     """
     _check_detectors_type_input(detectors)
     _md = {}
-    if md is None:
+    if md is None and not run_agnostic:
         md = get_stock_md({})
+        get_det_md(md, [detectors])
     _md.update(md or {})
 
     predeclare = per_step is None and os.environ.get("BLUESKY_PREDECLARE", False)
@@ -834,7 +837,7 @@ def mod_scan_nd(
     motors = list(cycler.keys)
 
     @stage_decorator(list(detectors) + motors)
-    @agnostic_run_decorator(run_agnostic)
+    @agnostic_run_decorator(run_agnostic, md=md)
     def inner_scan_nd():
         if predeclare:
             yield from bps.declare_stream(*motors, *detectors, name="primary")
@@ -842,3 +845,163 @@ def mod_scan_nd(
             yield from per_step(detectors, step, pos_cache)
 
     return (yield from inner_scan_nd())
+
+
+def mod_grid_scan(
+                  detectors: Sequence[Readable],
+                  *args,
+                 snake_axes: Optional[Union[Iterable, bool]] = None,
+                 per_step: Optional[PerStep] = None,
+                 md: Optional[CustomPlanMetadata] = None,
+                 run_agnostic=False,
+                 ) -> MsgGenerator[str]:
+    """
+    Scan over a mesh; each motor is on an independent trajectory.
+
+    Parameters
+    ----------
+    detectors: list or tuple
+        list of 'readable' objects
+    ``*args``
+        patterned like (``motor1, start1, stop1, num1,``
+                        ``motor2, start2, stop2, num2,``
+                        ``motor3, start3, stop3, num3,`` ...
+                        ``motorN, startN, stopN, numN``)
+
+        The first motor is the "slowest", the outer loop. For all motors
+        except the first motor, there is a "snake" argument: a boolean
+        indicating whether to following snake-like, winding trajectory or a
+        simple left-to-right trajectory.
+    snake_axes: boolean or iterable, optional
+        which axes should be snaked, either ``False`` (do not snake any axes),
+        ``True`` (snake all axes) or a list of axes to snake. "Snaking" an axis
+        is defined as following snake-like, winding trajectory instead of a
+        simple left-to-right trajectory. The elements of the list are motors
+        that are listed in `args`. The list must not contain the slowest
+        (first) motor, since it can't be snaked.
+    per_step: callable, optional
+        hook for customizing action of inner loop (messages per step).
+        See docstring of :func:`bluesky.plan_stubs.one_nd_step` (the default)
+        for details.
+    md: dict, optional
+        metadata
+    run_agnostic : bool, optional
+        If True, a new run will not be created.
+
+    See Also
+    --------
+    :func:`bluesky.plans.rel_grid_scan`
+    :func:`bluesky.plans.inner_product_scan`
+    :func:`bluesky.plans.scan_nd`
+    """
+    # Notes: (not to be included in the documentation)
+    #   The deprecated function call with no 'snake_axes' argument and 'args'
+    #         patterned like (``motor1, start1, stop1, num1,``
+    #                         ``motor2, start2, stop2, num2, snake2,``
+    #                         ``motor3, start3, stop3, num3, snake3,`` ...
+    #                         ``motorN, startN, stopN, numN, snakeN``)
+    #         The first motor is the "slowest", the outer loop. For all motors
+    #         except the first motor, there is a "snake" argument: a boolean
+    #         indicating whether to following snake-like, winding trajectory or a
+    #         simple left-to-right trajectory.
+    #   Ideally, deprecated and new argument lists should not be mixed.
+    #   The function will still accept `args` in the old format even if `snake_axes` is
+    #   supplied, but if `snake_axes` is not `None` (the default value), it overrides
+    #   any values of `snakeX` in `args`.
+
+    _check_detectors_type_input(detectors)
+    args_pattern = plan_patterns.classify_outer_product_args_pattern(args)
+    if (snake_axes is not None) and (args_pattern == plan_patterns.OuterProductArgsPattern.PATTERN_2):
+        raise ValueError(
+            "Mixing of deprecated and new API interface is not allowed: "
+            "the parameter 'snake_axes' can not be used if snaking is "
+            "set as part of 'args'"
+        )
+
+    # For consistency, set 'snake_axes' to False if new API call is detected
+    if (snake_axes is None) and (args_pattern != plan_patterns.OuterProductArgsPattern.PATTERN_2):
+        snake_axes = False
+
+    chunk_args = list(plan_patterns.chunk_outer_product_args(args, args_pattern))
+    # 'chunk_args' is a list of tuples of the form: (motor, start, stop, num, snake)
+    # If the function is called using deprecated pattern for arguments, then
+    # 'snake' may be set True for some motors, otherwise the 'snake' is always False.
+
+    # The list of controlled motors
+    motors = [_[0] for _ in chunk_args]
+
+    # Check that the same motor is not listed multiple times. This indicates an error in the script.
+    if len(set(motors)) != len(motors):
+        raise ValueError(f"Some motors are listed multiple times in the argument list 'args': '{motors}'")
+
+    if snake_axes is not None:
+
+        def _set_snaking(chunk, value):
+            """Returns the tuple `chunk` with modified 'snake' value"""
+            _motor, _start, _stop, _num, _snake = chunk
+            return _motor, _start, _stop, _num, value
+
+        if isinstance(snake_axes, collections.abc.Iterable) and not isinstance(snake_axes, str):
+            # Always convert to a tuple (in case a `snake_axes` is an iterator).
+            snake_axes = tuple(snake_axes)
+
+            # Check if the list of axes (motors) contains repeated entries.
+            if len(set(snake_axes)) != len(snake_axes):
+                raise ValueError(f"The list of axes 'snake_axes' contains repeated elements: '{snake_axes}'")
+
+            # Check if the snaking is enabled for the slowest motor.
+            if len(motors) and (motors[0] in snake_axes):
+                raise ValueError(f"The list of axes 'snake_axes' contains the slowest motor: '{snake_axes}'")
+
+            # Check that all motors in the chunk_args are controlled in the scan.
+            #   It is very likely that the script running the plan has a bug.
+            if any([_ not in motors for _ in snake_axes]):  # noqa: C419
+                raise ValueError(
+                    f"The list of axes 'snake_axes' contains motors "
+                    f"that are not controlled during the scan: "
+                    f"'{snake_axes}'"
+                )
+
+            # Enable snaking for the selected axes.
+            #   If the argument `snake_axes` is specified (not None), then
+            #   any `snakeX` values that could be specified in `args` are ignored.
+            for n, chunk in enumerate(chunk_args):
+                if n > 0:  # The slowest motor is never snaked
+                    motor = chunk[0]
+                    if motor in snake_axes:
+                        chunk_args[n] = _set_snaking(chunk, True)
+                    else:
+                        chunk_args[n] = _set_snaking(chunk, False)
+
+        elif snake_axes is True:  # 'snake_axes' has boolean value `True`
+            # Set all 'snake' values except for the slowest motor
+            chunk_args = [_set_snaking(_, True) if n > 0 else _ for n, _ in enumerate(chunk_args)]
+        elif snake_axes is False:  # 'snake_axes' has boolean value `True`
+            # Set all 'snake' values
+            chunk_args = [_set_snaking(_, False) for _ in chunk_args]
+        else:
+            raise ValueError(
+                f"Parameter 'snake_axes' is not iterable, boolean or None: "
+                f"'{snake_axes}', type: {type(snake_axes)}"
+            )
+
+    # Prepare the argument list for the `outer_product` function
+    args_modified = []
+    for n, chunk in enumerate(chunk_args):
+        if n == 0:
+            args_modified.extend(chunk[:-1])
+        else:
+            args_modified.extend(chunk)
+    full_cycler = plan_patterns.outer_product(args=args_modified)
+
+    _md = {}
+    if md is None and not run_agnostic:
+        md = get_stock_md({})
+        get_det_md(md, [detectors])
+    _md.update(md or {})
+
+    return (yield from mod_scan_nd(detectors,
+                                   full_cycler,
+                                   per_step=per_step,
+                                   md=_md,
+                                   run_agnostic=run_agnostic))
