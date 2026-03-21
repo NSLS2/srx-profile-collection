e9064edef60b0edac46db4d48a15440f8b0487ae
SRX Operator <xf05id1@xf05id2-ws7.nsls2.bnl.local>
2026-03-21 16:54:35 -0400

    Added compucentric pseudomotor and calibration math

--- startup/19-pseudomotors.py
+++ startup/19-pseudomotors.py
@@ -2,6 +2,7 @@ print(f'Loading {__file__}...')
 
 
 import numpy as np
+from scipy.optimize import curve_fit
 
 from ophyd import (
     EpicsSignal,
@@ -22,7 +23,7 @@ class ProjectedTopStage(PseudoPositioner):
     projx = Cpt(PseudoSingle)
     projz = Cpt(PseudoSingle)
 
-    # Real axes. From XRXNanoStage class definition.
+    # Real axes. From SRXNanoStage class definition.
     topx = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:xth}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:xth}Mtr.RBV
     topz = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:zth}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:zth}Mtr.RBV
 
@@ -266,3 +267,238 @@ class ProjectedTopStage(PseudoPositioner):
 #     # print(f'Move to {ycen} ycen.')
 #     yield from mv(xmotor, xcen,
 #                   ymotor, ycen)
+
+
+# Not needed with working top stages
+class CompucentricRotation(PseudoPositioner):
+
+    # Pseudo axes
+    rotation = Cpt(PseudoSingle)
+
+    # Real axes
+    # Rotation
+    th = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:th}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:th}Mtr.RBV
+    # Coarse motors
+    x = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:sx}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:sx}Mtr.RBV
+    z = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:sz}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:sz}Mtr.RBV
+    # Fine motors
+    # sx = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:ssx}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:ssx}Mtr.RBV
+    # sz = Cpt(EpicsMotor, 'XF:05IDD-ES:1{nKB:Smpl-Ax:ssz}Mtr')  # XF:05IDD-ES:1{nKB:Smpl-Ax:ssz}Mtr.RBV
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.r = 0
+        self.th0 = 0
+        self._mode = 'coarse'
+
+    
+    @property
+    def mode(self):
+        return self._mode
+
+    
+    def calculate_deltas(self, new_th):
+        # new_th in mdeg
+
+        curr_th = self.real_position.th # in mdeg
+        curr_phi = np.radians((curr_th + self.th0) / 1e3)
+        new_phi = np.radians((new_th + self.th0) / 1e3)
+        # phi in radians
+
+        # Negative dx for some reason
+        dx = -self.r * (np.cos(new_phi) - np.cos(curr_phi))
+        dz = self.r * (np.sin(new_phi) - np.sin(curr_phi))
+
+        return dx, dz
+
+
+    def set_mode(self, key='fine'):
+        if key.lower() == 'fine':
+            self._mode = 'fine'
+        elif key.lower() == 'coarse':
+            self._mode = 'coarse'
+        else:
+            raise ValueError(f"Unknown motor key {key}. Only 'fine' and 'coarse' accepted.")
+        
+    
+    @pseudo_position_argument
+    def forward(self, new_th):
+
+        dx, dz = self.calculate_deltas(new_th.rotation)
+        return self.RealPosition(x=self.real_position.x + dx,
+                                 z=self.real_position.z + dz,
+                                 th=new_th.rotation)
+
+
+    @real_position_argument
+    def inverse(self, real_pos):
+        return self.PseudoPosition(rotation=real_pos.th)
+    
+
+
+        # if self.mode == 'coarse':
+        #     return self.RealPosition(x=self.real_position.x + dx,
+        #                              z=self.real_position.z + dz,
+        #                              th=new_th)
+        # elif self.mode == 'fine':
+        #     return self.RealPosition(x=self.real_position.sx + dx,
+        #                              z=self.real_position.sz + dz,
+        #                              th=new_th)
+
+
+# Should behave just like th until the calibration function has been ran
+comp_th = CompucentricRotation(name='compucentric_th')
+
+
+# Compucentric fitting math lives here in case the measurements are taken manually
+
+# Functional form of x offsets from perfectly cirucular motion
+def compucentric_x_model(th, r, th0, x0):
+    # Negative for some unkown reason???
+    return -r * np.cos(th + th0) + x0
+
+
+# Functional form of x offsets from perfectly cirucular motion
+def compucentric_z_model(th, r, th0, x0):
+    return r * np.sin(th + th0) + x0
+
+
+# Combined offsets
+def compucentric_combined_model(th, r, th0, x0, z0):
+    x = compucentric_x_model(th, r, th0, x0)
+    z = compucentric_z_model(th, r, th0, z0)
+    return np.concatenate([x, z])
+
+
+def fit_compucentric_model(th, x_obs=None, z_obs=None,
+                           correct_pseudomotor=True,
+                           plot=True):
+
+    bounds = [[0, -np.pi, -np.inf],
+              [np.inf, np.pi, np.inf]]
+    observed = None
+    labels = []
+    if x_obs is not None:
+        observed = 'x'
+        labels.append('x0')
+        if len(th) != len(x_obs):
+            raise ValueError('All observations must have same length.')
+        obs = x_obs
+        model = compucentric_x_model
+        p0 = [(np.max(x_obs) - np.min(x_obs)) / 2,
+              0, # overwritten
+              np.mean(x_obs)]
+    if z_obs is not None:
+        observed = 'z'
+        labels.append('z0')
+        if len(th) != len(z_obs):
+            raise ValueError('All observations must have same length.')
+        obs = z_obs
+        model = compucentric_z_model
+        p0 = [(np.max(z_obs) - np.min(z_obs)) / 2,
+              0, # overwritten
+              np.mean(z_obs)]
+    if x_obs is not None and z_obs is not None:
+        observed = 'both'
+        obs = np.concatenate([x_obs, z_obs])
+        model = compucentric_combined_model
+        p0 = [np.mean([(np.max(x_obs) - np.min(x_obs)) / 2,
+                       (np.max(z_obs) - np.min(z_obs)) / 2]),
+              0,
+              np.mean(x_obs), np.mean(z_obs)
+              ]
+        bounds[0].append(-np.inf)
+        bounds[1].append(np.inf)
+    if observed is None:
+        raise ValueError('Must define one or both of x_obs or z_obs.')
+
+    succ, fits, errs, r_sqr = [], [], [], []
+    for rad in np.radians([-180, 0, 180]):
+        p0[1] = rad
+
+        try:
+            popt, pcov = curve_fit(model, th, obs, p0=p0, bounds=bounds)
+            succ.append(True)
+        except Exception as e:
+            succ.append(False)
+            err = e
+            popt = p0
+            pcov = np.eye(len(p0)) * 1e10
+        
+        perr = np.sqrt(np.diag(pcov))
+        fits.append(popt)
+        errs.append(perr)
+        
+        # Calculate r_squared
+        obs_pred = model(th, *popt)
+        res = obs - obs_pred
+        ss_res = np.sum(res**2)
+        ss_tot = np.sum((obs - np.mean(obs))**2)
+        if ss_tot != 0:
+            r_squared = 1 - (ss_res / ss_tot)
+        else:
+            r_squared = 0
+        r_sqr.append(r_squared)
+
+    if not any(succ):
+        # raise ValueError('All fitting failed.')
+        print(f'All Fitting Failed: {err}')
+
+    # print([p[0] for p in fits])
+    # print([np.degrees(p[1]) for p in fits])
+    # print(r_sqr)
+
+    best_idx = np.argmax(r_sqr)
+    r_fit, th0_fit = fits[best_idx][:2]
+    r_err, th0_err = errs[best_idx][:2]
+    r_rel_err = (r_err / r_fit) * 100 if r_fit != 0 else np.inf
+
+    print('Fit Results:')
+    print(f'R-squred is {r_sqr[best_idx]:.7f}')
+    print(f'\tradius = {r_fit:.3f} ± {r_err:.3f} μm')
+    print(f'\tth0 = {np.degrees(th0_fit):.3f} ± {np.degrees(th0_err):.3f} deg')
+
+    for i, (off_fit, off_err) in enumerate(zip(fits[best_idx][2:],
+                                               errs[best_idx][2:])):
+        print(f'\t{labels[i]} = {off_fit:.3f} ± {off_err:.3f} μm')
+
+    if plot:
+        fit_th = np.linspace(np.min(th), np.max(th), 5 * len(th))
+        if observed == 'both':
+            x_pred, z_pred = np.split(model(fit_th, *fits[best_idx]), 2)
+        elif observed == 'x':
+            x_pred = model(fit_th, *fits[best_idx])
+        elif observed == 'z':
+            z_pred = model(fit_th, *fits[best_idx])
+
+        if x_obs is not None:
+            fig, ax = plt.subplots()
+            ax.scatter(np.degrees(th), x_obs, c='r', label='Measured')
+            ax.plot(np.degrees(fit_th), x_pred, c='k', label='Fit')
+            ax.set_xlabel('Angle [deg]')
+            ax.set_ylabel('X Position [μm]')
+            ax.set_title('Compucentric X position Fit')
+            ax.legend()
+            fig.show()
+        if z_obs is not None:
+            fig, ax = plt.subplots()
+            ax.scatter(np.degrees(th), z_obs, c='r', label='Measured')
+            ax.plot(np.degrees(fit_th), z_pred, c='k', label='Fit')
+            ax.set_xlabel('Angle [deg]')
+            ax.set_ylabel('Z Position [μm]')
+            ax.set_title('Compucentric Z position Fit')
+            ax.legend()
+            fig.show()
+
+
+    if r_rel_err > 10:
+        print(('WARNING: Poor calibration. Relative error of radius greater than 10%.'
+                + '\nLarger angular range may improve fitting.'))
+
+    if correct_pseudomotor:
+        comp_th.r = r_fit
+        comp_th.th0 = np.degrees(th0_fit) * 1e3
+    else:
+        return fits[best_idx]
